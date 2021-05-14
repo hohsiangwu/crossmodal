56d1555682b5b9786db4bb6a1efd2e85128996bc
Ho-Hsiang Wu <hohsiangwu@users.noreply.github.com>
2021-05-14 15:16:59 -0400

    fix )

--- crossmodal/embedding.py
+++ crossmodal/embedding.py
@@ -64,7 +64,7 @@ def extract_vgg(X, batch_size=1024):
 
 @click.command()
 @click.option('--input_dir', required=True, help='Input folder.')
-@click.option('--algo', type=click.Choice(['openl3', 'yamnet', 'resnet', 'vgg'], required=True, help='Embedding algorithms.'))
+@click.option('--algo', type=click.Choice(['openl3', 'yamnet', 'resnet', 'vgg']), required=True, help='Embedding algorithms.')
 @click.option('--output_dir', required=True, help='Output folder.')
 def extract_embedding(input_dir, algo, output_dir):
     files = sorted(glob.glob('{}/*'.format(input_dir)))
